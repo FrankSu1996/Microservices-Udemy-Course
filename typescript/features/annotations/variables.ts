a55442168453dd1249465c9a1904070187e8e10e
Frank Su <franksu49@gmail.com>
2021-01-31 13:20:56 -0500

    added scenarios when type annotations are necessary for variables

--- typescript/features/annotations/variables.ts
+++ typescript/features/annotations/variables.ts
@@ -22,3 +22,35 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+// Function: lhs of equal sign is annotation, rhs is actual function definition
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); //{x: 10, y: 20}
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+for (let word in words) {
+  if (word === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+// add | to type annotation when variable can have more than 1 type
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
