efd2398c2fbbf674bcf752faffdc7be7aac649d8
Frank Su <franksu49@gmail.com>
2021-01-31 12:34:24 -0500

    started looking at ts feastures

---
+++ typescript/features/annotations/variables.ts
@@ -0,0 +1,9 @@
+// define variables with type annotations
+let apples: number = 5;
+let speed: string = "fast";
+let hastName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
---
+++ typescript/features/types.ts
@@ -0,0 +1,14 @@
+const today = new Date();
+
+// can get methods and attributes with autocomplete
+today.getMonth();
+
+// creating new object, since we didn't have type annotation hovering
+// person will just print out entire object
+const person = {
+  age: 20,
+};
+
+class Color {}
+
+const red = new Color();
