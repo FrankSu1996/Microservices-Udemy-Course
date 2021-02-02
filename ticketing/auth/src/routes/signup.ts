6edacfbdabc5117f24e210e2333783c6ad9ff38d
Frank Su <franksu49@gmail.com>
2021-02-02 18:15:01 -0500

    added request validation in signup route

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,9 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Hi there!");
-});
+router.post(
+  "/api/users/signup",
+  // express validator middleware checks that email and password are valid
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    // pull off errors in req object
+    const errors = validationResult(req);
+
+    // check to see if errors are present and handle
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log("Creating a user...");
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
