aa728035db1ea88715ef2cfd43de04fb3eda6a59
Frank Su <franksu49@gmail.com>
2021-02-02 18:59:47 -0500

    created error handler middleware

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -4,13 +4,19 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 app.use(json());
+
+// route handlers middleware
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// error handler middleware
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
