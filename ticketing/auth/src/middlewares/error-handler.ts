4d5d2867cfbc8bd2b353dabbad31f54b053570f1
Frank Su <franksu49@gmail.com>
2021-02-03 18:20:09 -0500

    implemented error check in error handler

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,27 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  // check what error comes in
+  if (err instanceof RequestValidationError) {
+    // map over each error, return them as properly formatted array of error objects
+    const formattedErrors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    // response 400 = user sent bad request data
+    return res.status(400).send({ errors: formattedErrors });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    // response 500 = internal server error
+    return res.status(500).send({
+      errors: [
+        {
+          message: err.reason,
+        },
+      ],
+    });
+  }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: "Something went wrong" }],
   });
 };
