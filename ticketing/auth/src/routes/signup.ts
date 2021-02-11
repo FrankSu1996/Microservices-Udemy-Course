d9aa7c848bddec80501c05af1dfe1865a5510c7d
Frank Su <franksu49@gmail.com>
2021-02-11 17:17:07 -0500

    finished signIn route handler

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { body } from "express-validator";
 import { BadRequestError } from "../errors/bad-request-error";
 import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
@@ -17,15 +17,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    // pull off errors in req object
-    const errors = validationResult(req);
-
-    // check to see if errors are present and handle
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     // check to see if a user already exists in db
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
